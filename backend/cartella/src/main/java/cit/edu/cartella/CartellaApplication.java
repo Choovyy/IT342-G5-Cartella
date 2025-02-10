1655f96672fa44642d28a0d7ae85f37b0d3d0eae
Choovyy <tovijoshua.hermosisima@cit.edu>
2025-02-10 02:03:09 +0000

    Folders created

---
+++ backend/cartella/src/main/java/cit/edu/cartella/CartellaApplication.java
@@ -0,0 +1,13 @@
+package cit.edu.cartella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartellaApplication.class, args);
+	}
+
+}
